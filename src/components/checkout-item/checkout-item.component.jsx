9eb10e8c5ee75a88ee1a432145bfeedccc8c04cb
Ramin Mammadzada <remki-design@hotmail.com>
2021-04-17 13:31:52 +0300

    Add addItem and removeItem functionality to checkout item component.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,28 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { addItem, removeItem, clearItemFromCart } from '../../redux/cart/cart.actions';
+
 import './checkout-item.styles.scss';
 
 const CheckoutItem = ({
-  cartItem: {
+  cartItem,
+  clearItem,
+  addItem,
+  removeItem,
+}) => {
+  const {
     name,
     imageUrl,
     price,
     quantity,
-  },
-}) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
+  } = cartItem;
+
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <div className="quantity">
+        <div
+          className="arrow"
+          onClick={() => removeItem(cartItem)}
+          aria-hidden="true"
+        >
+          &#10094;
+        </div>
+        <span className="value">
+          {quantity}
+        </span>
+        <div
+          className="arrow"
+          onClick={() => addItem(cartItem)}
+          aria-hidden="true"
+        >
+          &#10095;
+        </div>
+      </div>
+
+      <span className="price">{price}</span>
+      <div
+        className="remove-button"
+        onClick={() => clearItem(cartItem)}
+        role="button"
+        tabIndex="0"
+        aria-hidden="true"
+      >
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove">&#10005;</div>
-  </div>
-);
+  );
+};
 
 CheckoutItem.propTypes = {
   cartItem: PropTypes.objectOf(PropTypes.any).isRequired,
+  clearItem: PropTypes.func.isRequired,
+  addItem: PropTypes.func.isRequired,
+  removeItem: PropTypes.func.isRequired,
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
