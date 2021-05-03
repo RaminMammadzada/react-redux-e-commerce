27d6c44705881b44427093181dec5201d43531d0
Ramin Mammadzada <remki-design@hotmail.com>
2021-05-03 04:15:57 +0300

    Move sign up functionality to user saga file.

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -16,6 +16,8 @@ import {
   signInFailure,
   signOutSuccess,
   signOutFailure,
+  signUpFailure,
+  signUpSuccess,
 } from './user.actions';
 
 export function* getSnapshotFromUserAuth(userAuth) {
@@ -68,6 +70,16 @@ export function* signOut() {
   }
 }
 
+export function* signUp({ payload: { email, password, displayName } }) {
+  try {
+    const { user } = yield auth.createUserWithEmailAndPassword(email, password);
+    yield getSnapshotFromUserAuth({ ...user, displayName });
+    yield put(signUpSuccess());
+  } catch (error) {
+    yield put(signUpFailure(error));
+  }
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -84,11 +96,16 @@ export function* onSignOutStart() {
   yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
 }
 
+export function* onSignUpStart() {
+  yield takeLatest(UserActionTypes.SIGN_UP_START, signUp);
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
     call(onCheckUserSession),
     call(onSignOutStart),
+    call(onSignUpStart),
   ]);
 }
